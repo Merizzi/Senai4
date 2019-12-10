3cf103c25ddbc231539a2c4cfe54b275cf4420c7
Pablo Merissi <pablo.merissi@hotmail.com>
2019-12-10 12:13:15 -0200

    Acerto do Logoff inicio cad evento

--- RoleTopMVC/Models/Orcamento.cs
+++ RoleTopMVC/Models/Orcamento.cs
@@ -1,7 +1,22 @@
+using System;
+using Microsoft.Extensions.Primitives;
+
 namespace RoleTopMVC.Models
 {
     public class Orcamento
     {
-        
+        public string Evento {get;set;}
+
+        public Cliente Cliente {get;set;}
+        public DataTime Evento {get;set;}
+        public Time Horario {get;set} 
+        public string Descricao {get;set;}
+        public string Celular {get;set;}
+
+        public Orcamento()
+        {
+            this.Cliente = new Cliente();
+            
+        }
     }
 }
---
+++ RoleTopMVC/Repositories/OrcamentoRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+using RoleTopMVC.Models;
+
+namespace RoleTopMVC.Repositories
+{
+    public class OrcamentoRepository : RepositoryBase
+    {
+        
+    }
+}
